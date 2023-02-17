b2ffafaa2e0b7aea0b6948caa118c43f34a7e081
unknown <albinglindell98@gmail.com>
2023-02-17 17:58:38 +0100

    removed som unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import "./styles/App.scss"
-import { Route, Routes } from 'react-router-dom';
-import ImagesComponent from './components/ImagesComponent';
-import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
